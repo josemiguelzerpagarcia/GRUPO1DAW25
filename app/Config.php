ad8a20db95d92179b7cfa01a33d3a361f67e7c05
Jose Miguel Zerpa Garcia <zerpa.garcia.jose.miguel@iescamas.es>
2025-12-04 12:28:02 +0100

    modificacion del config.php

--- app/Config.php
+++ app/Config.php
@@ -5,7 +5,7 @@
      static public $mvc_bd_hostname = "localhost";
      static public $mvc_bd_nombre   = "forbids_alimentos12025";
      static public $mvc_bd_usuario  = "forbids";
-     static public $mvc_bd_clave    = "88!GQs.J4vtQ5Qq";
+     static public $mvc_bd_clave    = "grupo1DAW";
      static public $mvc_vis_css     = "estilo.css";
  }
 ?>
